7c28425451cdef28d1d8b93096f8064c34e21531
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-06-01 01:13:07 +0530

    Used contextAPI and hooks to refactor

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,20 @@
-import React, { Component, Fragment } from 'react'
+import React, { useEffect, useContext, Fragment } from 'react'
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 import { Link } from 'react-router-dom';
 
-export class User extends Component {
-    componentDidMount(){
-        console.log(this.props)
-        this.props.getUser(this.props.match.params.login)
-        this.props.getUserRepos(this.props.match.params.login)
-    }
+const User = ({ match }) => {
+
+    const githubContext = useContext(GithubContext);
+
+    const { user, loading, getUser, getUserRepos, repos } = githubContext
     
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired,
-        repos: PropTypes.array.isRequired
-    }
-    render() { 
+    useEffect(() => {
+        getUser(match.params.login)
+        getUserRepos(match.params.login)
+        // eslint-disable-next-line
+    }, []);
         const {
             name,
             avatar_url,
@@ -33,9 +29,7 @@ export class User extends Component {
             public_repos,
             public_gists,
             hireable
-        } = this.props.user;
-
-        const {loading} = this.props
+        } = user;
 
         if (loading) return <Spinner />
         
@@ -86,10 +80,9 @@ export class User extends Component {
                   <div className="badge badge-light">Public Repos: {public_repos}</div>
                   <div className="badge badge-dark">Public Gists: {public_gists}</div>
                 </div>
-                <Repos repos={this.props.repos} />
+                <Repos repos={repos} />
             </Fragment>
         )
-    }
 }
 
 export default User
